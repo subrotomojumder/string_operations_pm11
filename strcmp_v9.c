10a3450b79db8c09f7fbac71da2659e3705255e9
Janardhan-Majumder <janardhan.md03@gmail.com>
2025-10-28 19:55:44 +0600

    strcmp practice

---
+++ strcmp_v9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[101], b[101];
+    scanf("%s %s", &a, &b);
+    int val = strcmp(a, b);
+    // printf("%d", val);
+    if (val < 0)
+    {
+        printf("A is smaller");
+    }
+    else if (val == 0)
+    {
+        printf("A & B is Equal");
+    }
+    else if (val > 0)
+    {
+        printf("B is smaller");
+    }
+
+    return 0;
+}
